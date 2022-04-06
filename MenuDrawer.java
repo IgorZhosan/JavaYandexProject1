173d2468071ab5af42156aae24e2c9bb4791b058
Igor <cool.eccentric@yandex.ru>
2022-04-07 00:05:55 +0300

    Последний код-ревью точноgit status

--- MenuDrawer.java
+++ MenuDrawer.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuDrawer {
@@ -10,40 +11,45 @@ public class MenuDrawer {
 
         while (true) {
 
-            byte command = scanner.nextByte();
-            if (command == 1) {
-                System.out.println("Введите месяц");
-                String monthName = scanner.next();
-                System.out.println("За какой день будем добавлять количество шагов?");
-                byte day = scanner.nextByte();
-                System.out.println("Введите количество шагов");
-                int step = scanner.nextInt();
 
-                stepTracker.saveStep(monthName, day, step);
+                String command = scanner.next();
 
-            } else if (command == 2) {
-                System.out.println("Введите месяц");
-                String monthName = scanner.next();
-                stepTracker.printStep(monthName);
-                System.out.println("Общее количество шагов за месяц " + monthName.toLowerCase() + " " + stepTracker.countStep(monthName));
-                System.out.println("Максимальное пройденное количество шагов в месяце " + monthName.toLowerCase() + " " + stepTracker.maxMonthStep(monthName));
-                System.out.println("Среднее количество шагов за месяц " + monthName.toLowerCase() + " " + stepTracker.averageSteps(monthName));
-                System.out.println("Пройденная дистанция за месяц " + monthName.toLowerCase() + " " + converter.converterSteps(monthName));
-                System.out.println("Сожженных ккал за месяц " + monthName.toLowerCase() + " " + converter.converterCalories(monthName));
-                System.out.println("Лучшая серия подряд идущих дней месяца " + monthName.toLowerCase() + ", шаги которого были выше целевого: " + stepTracker.bestSeries(monthName));
-            } else if (command == 3) {
-                System.out.println("Какое значение станет целевым за месяц?");
-                double step = scanner.nextDouble();
-                stepTracker.setStepMonth(step);
-                System.out.println("Целевое значение изменилось на: " + stepTracker.getStepMonth());
-            } else if (command == 4) {
-                break;
-            } else
-                System.out.println("Неверная команда");
-            printMenu();
 
+                if (command.contains("1")) {
+                    System.out.println("Введите месяц");
+                    String monthName = scanner.next();
+                    System.out.println("За какой день будем добавлять количество шагов?");
+                    byte day = scanner.nextByte();
+                    System.out.println("Введите количество шагов");
+                    int step = scanner.nextInt();
+
+                    stepTracker.saveStep(monthName, day, step);
+
+                } else if (command.contains("2")) {
+                    System.out.println("Введите месяц");
+                    String monthName = scanner.next();
+                    stepTracker.printStep(monthName);
+                    System.out.println("Общее количество шагов за месяц " + monthName.toLowerCase() + " " + stepTracker.countStep(monthName));
+                    System.out.println("Максимальное пройденное количество шагов в месяце " + monthName.toLowerCase() + " " + stepTracker.maxMonthStep(monthName));
+                    System.out.println("Среднее количество шагов за месяц " + monthName.toLowerCase() + " " + stepTracker.averageSteps(monthName));
+                    System.out.println("Пройденная дистанция за месяц " + monthName.toLowerCase() + " " + converter.converterSteps(monthName));
+                    System.out.println("Сожженных ккал за месяц " + monthName.toLowerCase() + " " + converter.converterCalories(monthName));
+                    System.out.println("Лучшая серия подряд идущих дней месяца " + monthName.toLowerCase() + ", шаги которого были выше целевого: " + stepTracker.bestSeries(monthName));
+                } else if (command.contains("3")) {
+                    System.out.println("Какое значение станет целевым за месяц?");
+                    double step = scanner.nextDouble();
+                    stepTracker.setStepMonth(step);
+                    System.out.println("Целевое значение изменилось на: " + stepTracker.getStepMonth());
+                } else if (command.contains("4")) {
+                    break;
+                } else
+                    System.out.println("Неверная команда, введите число от 1 до 4");
+
+                printMenu();
+
+            }
         }
-    }
+
 
     public static void printMenu() {
         System.out.println("1 - Ввести количество шагов за определённый день");
